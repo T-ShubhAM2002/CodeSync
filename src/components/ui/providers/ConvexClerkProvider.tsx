054448312d5e3fa21e589ea38b9009acac484d50
T Shubham <shubham2003march25@gmail.com>
2025-03-03 23:20:12 +0530

    changes to webhooks

--- src/components/ui/providers/ConvexClerkProvider.tsx
+++ src/components/ui/providers/ConvexClerkProvider.tsx
@@ -5,13 +5,13 @@ import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!); // creates a Convex client instance with the provided URL
 
-function ConvexClerkProvider({ children }: { children: React.ReactNode }) {
+function ConvexClerkProvider({ children }: { children: React.ReactNode }) {// wrapper component that provides both Convex and Clerk providers
     return (
-        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-                {children}
+        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}> {/* provides Clerk authentication */}
+            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>  {/* provides Convex client and Clerk authentication uses useAuth to check user identity */}
+                {children} {/* Ensures all nested components get access to Clerk and Convex */}
             </ConvexProviderWithClerk>
         </ClerkProvider>
     );
